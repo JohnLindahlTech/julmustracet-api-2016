8e20975e24fa1796addb9425b5b1933528bb967d
John Bäckström <john.backstrom@nordnet.se>
2016-10-13 06:56:38 +0200

    Added total-calculations for Brand and Player

--- common/models/brand.js
+++ common/models/brand.js
@@ -1,5 +1,32 @@
 'use strict';
 
 module.exports = function(Brand) {
+  Brand.observe('loaded', function(ctx, next) {
+    if (ctx.instance) {
+      var sum = 0;
 
+      Brand.app.models.Drink.find({
+        where: {
+          brandId: ctx.instance.id,
+        },
+        fields: {
+          amount: true,
+        },
+      }, function(err, drinks) {
+        if (err) return next(err);
+
+        if (drinks.length) {
+          drinks.forEach(function(drink) {
+            sum += drink.amount;
+          });
+
+          ctx.instance.total = sum;
+        }
+
+        return next();
+      });
+    } else {
+      return next();
+    }
+  });
 };
--- common/models/player.js
+++ common/models/player.js
@@ -1,5 +1,32 @@
 'use strict';
 
 module.exports = function(Player) {
+  Player.observe('loaded', function(ctx, next) {
+    if (ctx.instance) {
+      var sum = 0;
 
+      Player.app.models.Drink.find({
+        where: {
+          playerId: ctx.instance.id,
+        },
+        fields: {
+          amount: true,
+        },
+      }, function(err, drinks) {
+        if (err) return next(err);
+
+        if (drinks.length) {
+          drinks.forEach(function(drink) {
+            sum += drink.amount;
+          });
+
+          ctx.instance.total = sum;
+        }
+
+        return next();
+      });
+    } else {
+      return next();
+    }
+  });
 };
